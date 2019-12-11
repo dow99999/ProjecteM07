5c5b85f926646ae619ef6e660b4544c9ea6618fa
dow99999 <38563257+dow99999@users.noreply.github.com>
2019-12-11 10:57:35 +0100

    Handle de botones hecho, falta comprobacion de usuario + creacion de pantalla principal

--- app/src/main/java/com/munoz/diego/projectem07/LoginActivity.java
+++ app/src/main/java/com/munoz/diego/projectem07/LoginActivity.java
@@ -2,13 +2,38 @@ package com.munoz.diego.projectem07;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class LoginActivity extends AppCompatActivity {
+
+    private EditText m_user;
+    private EditText m_pass;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        m_user = findViewById(R.id.etUser);
+        m_pass = findViewById(R.id.etPassword);
+
+    }
+
+    public void handleRegister(View view){
+        Intent intent = new Intent(this, RegisterActivity.class);
+        startActivity(intent);
+    }
+
+    public void handleLogin(View view){
+
+        //TODO hacer la comprobacion del usuario/password
+
+        Intent intent = new Intent(this, RegisterActivity.class);
+        intent.putExtra("user", m_user.getText().toString());
+        startActivity(intent);
     }
 }
