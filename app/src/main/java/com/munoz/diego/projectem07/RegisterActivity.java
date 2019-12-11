4b9f326cbfbf40244624f41f1ba1c1e575ac3261
Arnau <arnau.muro@gmail.com>
2019-12-11 10:56:16 +0100

    Merge branch 'master' of https://github.com/dow99999/ProjecteM07
    # Conflicts:
    #	app/src/main/java/com/munoz/diego/projectem07/LoginActivity.java

--- app/src/main/java/com/munoz/diego/projectem07/RegisterActivity.java
+++ app/src/main/java/com/munoz/diego/projectem07/RegisterActivity.java
@@ -2,10 +2,15 @@ package com.munoz.diego.projectem07;
 
 import android.os.Bundle;
 
-public class RegisterActivity {
+import androidx.appcompat.app.AppCompatActivity;
+
+public class RegisterActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        m
+
     }
 }
