6222a66f29a4b0797f7fcfddffd9ca392a7f4cb4
Diego Muñoz Jorquera <munoz.jorquera.diego@alumnat.copernic.cat>
2020-01-19 16:40:56 +0100

    Fragment para postear con diferentes controled de entrada

--- app/src/main/java/com/munoz/diego/projectem07/ui/gallery/GalleryFragment.java
+++ app/src/main/java/com/munoz/diego/projectem07/ui/gallery/GalleryFragment.java
@@ -4,6 +4,9 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 import android.widget.TextView;
 
 import androidx.annotation.Nullable;
@@ -14,22 +17,40 @@ import androidx.lifecycle.ViewModelProviders;
 
 import com.munoz.diego.projectem07.R;
 
-public class GalleryFragment extends Fragment {
+public class GalleryFragment extends Fragment implements AdapterView.OnItemSelectedListener {
 
     private GalleryViewModel galleryViewModel;
+
+    String[] m_animales;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         galleryViewModel =
                 ViewModelProviders.of(this).get(GalleryViewModel.class);
         View root = inflater.inflate(R.layout.fragment_gallery, container, false);
-        final TextView textView = root.findViewById(R.id.text_gallery);
-        galleryViewModel.getText().observe(this, new Observer<String>() {
-            @Override
-            public void onChanged(@Nullable String s) {
-                textView.setText(s);
-            }
-        });
+
+        m_animales = getResources().getStringArray(R.array.animales);
+
+        Spinner spin = root.findViewById(R.id.sp_animal);
+        spin.setOnItemSelectedListener(this);
+
+        ArrayAdapter aa = new ArrayAdapter(this.getContext(),android.R.layout.simple_spinner_item, m_animales);
+        aa.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+        spin.setAdapter(aa);
+
+
         return root;
+    }
+
+
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
+
     }
 }
